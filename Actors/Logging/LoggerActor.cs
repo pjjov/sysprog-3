66525aec1ae66319da2c6d7d9d224d0bf30daa77
Предраг Јовановић <102533878+pjjov@users.noreply.github.com>
2026-07-01 12:55:29 +0200

    Logovanje podataka

--- Actors/Logging/LoggerActor.cs
+++ Actors/Logging/LoggerActor.cs
@@ -11,10 +11,12 @@ public class LoggerActor : ReceiveActor
     {
         Receive<AddFile>(cmd => Context.ActorOf(Props.Create<FileLoggerActor>(cmd.filePath)));
         Receive<AddConsole>(cmd => Context.ActorOf(Props.Create<ConsoleLoggerActor>()));
-        Receive<Write<HttpHandlerActor.Response>>(write => HandleRequest(write.item));
-        Receive<Write<HttpHandlerActor.Request>>(write => HandleRequest(write.item));
-        Receive<Write<Exception>>(write => HandleRequest(write.item));
-        Receive<Write<string>>(write => HandleRequest(write.item));
+        Receive<Write<HttpHandlerActor.Response>>(write => Handle(write.item));
+        Receive<Write<HttpHandlerActor.Request>>(write => Handle(write.item));
+        Receive<Write<Exception>>(write => Handle(write.item));
+        Receive<Write<string>>(write => Handle(write.item));
+        Receive<Write<Prize>>(write => Handle(write.item));
+        Receive<Write<Laureate>>(write => Handle(write.item));
     }
 
     private void Broadcast(string category, string content)
@@ -25,22 +27,32 @@ public class LoggerActor : ReceiveActor
         }
     }
 
-    private void HandleRequest(HttpHandlerActor.Response res)
+    private void Handle(Prize p)
+    {
+        Broadcast("PRIZE", $"{p.id}: {p.year} {p.category} {p.prizeAmountAdjusted}");
+    }
+
+    private void Handle(Laureate l)
+    {
+        Broadcast("LAUREATE", $"{l.Id}: {l.Fullname}, {l.Category} {l.PrizeYear}");
+    }
+
+    private void Handle(HttpHandlerActor.Response res)
     {
         Broadcast("RESPONSE", $"{res.ctx.Request.HttpMethod} {res.ctx.Request.RawUrl} {res.ctx.Response.StatusCode}");
     }
 
-    private void HandleRequest(HttpHandlerActor.Request req)
-    {   
+    private void Handle(HttpHandlerActor.Request req)
+    {
         Broadcast("REQUEST", $"{req.ctx.Request.HttpMethod} {req.ctx.Request.RawUrl}");
     }
 
-    private void HandleRequest(Exception ex)
+    private void Handle(Exception ex)
     {
         Broadcast("ERROR", $"{ex.Message} \n{ex.StackTrace}");
     }
 
-    private void HandleRequest(string str)
+    private void Handle(string str)
     {
         Broadcast("MESSAGE", str);
     }
